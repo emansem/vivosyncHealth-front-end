a3803cc4e24351bf479429925149aefcef18b8e0
emansem <5nkolosite@gmail.com>
2024-12-11 21:21:22 +0100

    update transaction page

--- app/doctor/transaction/page.tsx
+++ app/doctor/transaction/page.tsx
@@ -150,7 +150,7 @@ export default function TransactionPage() {
             getTransactionIcon={getTransactionIcon}
             filteredTransactions={transactionsData as Transactions[]}
           />
-          {/* Pagination - Only shown when there are 10 or more items */}
+          {/* Pagination - Only shown when there are 10 or more items or the total pages is equal to pageNumber */}
           {shouldShowPagination && (
             <div className="px-6">
               <PaginationButton
@@ -165,6 +165,7 @@ export default function TransactionPage() {
             </div>
           )}
         </div>
+        <div>hello world</div>
 
         {/* Mobile View Component */}
         <TransactionMobileView
